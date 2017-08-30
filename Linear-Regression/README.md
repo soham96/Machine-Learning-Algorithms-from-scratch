ee58d9d6a3610e5878a2457e0216c762a24e19c2
soham96 <96soham96@gmail.com>
2017-08-30 22:22:14 +0530

    Update README

---
+++ Linear-Regression/README.md
@@ -0,0 +1,5 @@
+#Linear Regression from Scratch
+
+This is an implementation of Linear Regression using only numpy and pandas.
+To run this project: clone/download this and run
+linear-regression.py
--- Linear-Regression/linear-regression.py
+++ Linear-Regression/linear-regression.py
@@ -2,10 +2,19 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 
-def calc_error(m, c, x, y):
-       
-       totalError=0
+def visualise(x, y, m, c):
+       y_pred= [(m*i+c) for i in x]
 
+       plt.plot(x, y_pred, c='r', label='Predicted Values')
+       plt.scatter(x, y, c='g', label='Values')
+       plt.xlabel('Number of Crickets')
+       plt.ylabel('Temperature in Farenheit')
+       plt.legend(loc='best')
+       plt.show()
+
+
+def calc_error(m, c, x, y):
+       totalError=0
        for i in range(len(x)):
               totalError += (y[i] - (m * x[i] + c)) ** 2
 
@@ -17,21 +26,20 @@ def fit(x, y, epochs=50, learningrate=0.0001, initial_c=0, initial_m=0):
        N=len(x)
        new_m=initial_m
        new_c=initial_c
-       m=0
-       c=0
-
-
+       
        for i in range(epochs):
+              m=0
+              c=0
               for j in range(N):
                      m+= (-(2/float(N)) * x[j] * (y [j]- ((new_m * x[j]) + new_c)))
                      c+=(-(2/float(N)) * (y [j]- ((new_m * x[j]) + new_c)))
               new_m=new_m-(m*learningrate)
               new_c=new_c-(c*learningrate)
-              m=0
-              c=0
               error=calc_error(new_m, new_c, x, y)
               print("For Epoch {0} : slope={1} and y-intercept={2}".format(i, new_m, new_c))
               print("Mean Square Error = {0} \n".format(error))
+
+       visualise(x, y,  new_m, new_c)
 
 
 
